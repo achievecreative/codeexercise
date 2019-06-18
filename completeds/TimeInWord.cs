6cf43f68de8f15577292a083c8830558d3ca7fbb
Owen Chen <owen.chen@blackmores.com.au>
2019-06-18 16:25:00 +0800

    grid search

--- completeds/TimeInWord.cs
+++ completeds/TimeInWord.cs
@@ -2,38 +2,45 @@ using System;
 
 namespace Solutions
 {
-    public class Program{
-        static string[] hours = new []{"one", "two", "three", "four","five","six","seven","eight","nine","ten","eleven","twelve","thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
-
-        static string timeInWords(int h, int m) {
-            
-  if(m == 0){
-                return $"{hours[h-1]} o' clock";
+    public class Program
+    {
+        static string[] hours = new[] { "one", "two", "three", "four", "five", "six", "seven", "eight", "nine", "ten", "eleven", "twelve", "thirteen", "fourteen", "fifteen", "sixteen", "seventeen", "eighteen", "nineteen" };
+        static string timeInWords(int h, int m)
+        {
+            if (m == 0)
+            {
+                return $"{hours[h - 1]} o' clock";
             }
 
-            var middle = m > 30?"to":"past";
-            var hour = m > 30?h:h-1;
+            var middle = m > 30 ? "to" : "past";
+            var hour = m > 30 ? h : h - 1;
 
-             m = m > 30? (60 - m):m;
+            m = m > 30 ? (60 - m) : m;
 
             var minutes = "";
-            if(m == 30){
+            if (m == 30)
+            {
                 minutes = "half";
-            }else if(m == 15){
+            }
+            else if (m == 15)
+            {
                 minutes = "quarter";
             }
-            else if(m < 20){
-                minutes = hours[m-1] + (m==1?" minute":" minutes");
+            else if (m < 20)
+            {
+                minutes = hours[m - 1] + (m == 1 ? " minute" : " minutes");
             }
-            else{
+            else
+            {
                 minutes = "twenty";
 
                 var _m = m % 10;
-                if(_m != 0){
-                    minutes += " " + hours[_m-1];
+                if (_m != 0)
+                {
+                    minutes += " " + hours[_m - 1];
                 }
-                
-                minutes +=" minutes";
+
+                minutes += " minutes";
             }
 
             return $"{minutes} {middle} {hours[hour]}";
